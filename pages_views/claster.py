37a712a3cefcb84076e9bfd5a138df26ea7f52ec
Тарасов Артём Александрович <aatara57@mts.ru>
2024-10-02 22:54:38 +0300

    stable version for testing and redy to demo

---
+++ pages_views/claster.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+
+
+class ShowClasters:
+
+    def _display_content(self, df, cluster_count: int):
+        # Чтение данных из CSV строки
+        # df = pd.read_csv(pd.compat.StringIO(csv_data))
+        i = 0
+
+        grouped = df.groupby('cluster_id')
+
+        for cluster_id, group_data in grouped:
+            if (i < cluster_count):
+                s = "Кластер" + " " + '\'' + group_data['topic_name'].iloc[0] + '\''
+                with st.expander(s):
+                    for index, row in group_data.iterrows():
+                        answer = row['answer']
+                        st.write(f'Answer: {answer}')
+                        # Дополнительные данные о кластере, если они есть, могут быть отображены здесь
+                i += 1
+
+
